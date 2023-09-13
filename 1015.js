33cf5ea117a670269cdaaaa48a893ac1ff767f01
diogo.teixeira <diogoteixeira0602@gmail.com>
2023-09-12 21:30:36 -0300

    exercício 1015 resolvido

---
+++ 1015.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+const valores = input.split("\n");
+
+const [x1, y1] = valores[0].split(" ");
+const [x2, y2] = valores[1].split(" ");
+
+var distancia = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+
+console.log(distancia.toFixed(4))
