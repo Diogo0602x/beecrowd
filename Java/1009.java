dc51417079ad030b6bcbdb2f28c19e46d794cdc3
diogo.teixeira <diogoteixeira0602@gmail.com>
2023-09-14 14:39:18 -0300

    exercício 1009 resolvido java

---
+++ Java/1009.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args)  {
+    Scanner scanner = new Scanner(System.in);
+
+    String nome = scanner.next();
+    double salarioFixo = scanner.nextDouble();
+    double totalVendas = scanner.nextDouble();
+
+    double salarioTotal = salarioFixo + (totalVendas * 0.15);
+    
+    String salarioTotalFormatado = String.format("%.2f", salarioTotal);
+
+    System.out.println("TOTAL = R$ " + salarioTotalFormatado);
+  }
+}
