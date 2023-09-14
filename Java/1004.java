6290af1c31522ce129f9b35611fa0fee7f41b77a
diogo.teixeira <diogoteixeira0602@gmail.com>
2023-09-14 09:46:38 -0300

    exercício 1004 resolvido java

---
+++ Java/1004.java
@@ -0,0 +1,16 @@
+//1003
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int A = scanner.nextInt();
+    int B = scanner.nextInt();
+
+    int produto = A * B;
+
+    System.out.println("PROD = " + produto);
+  }
+
+}
